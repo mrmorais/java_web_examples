c4b80907be0e7f3848512fd2175ae8d9548c2941
Maradona Morais <maradona.morais@hotmail.com>
2019-04-10 09:57:28 -0300

    update basic 2

---
+++ JPA-Employee-Basic2/src/main/java/employee/data/EmployeeDAO.java
@@ -0,0 +1,51 @@
+package employee.data;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.UserTransaction;
+
+import employee.Employee;
+
+@Named
+@RequestScoped
+public class EmployeeDAO {
+	@PersistenceContext(unitName = "EmployeePU")
+	private EntityManager entityManager;
+
+	@Resource
+	private UserTransaction userTransaction;
+
+	public Employee addNew(Employee employee) {
+		try {
+			userTransaction.begin();
+			entityManager.persist(employee);
+			userTransaction.commit();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		return employee;
+	}
+
+	public List<Employee> findEmployees() {
+		TypedQuery<Employee> query = entityManager.createNamedQuery("findAllEmployees", Employee.class);
+		return query.getResultList();
+	}
+	
+	public void removeEmployee(Employee employee) {
+		try {
+			userTransaction.begin();
+			entityManager.remove(employee);
+			userTransaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}

136b34f35880825ff7fce3a4ef3c0f5cd4b85a78
Francisco Maradona de Freitas Morais <mrmorais@A304-09L.imd.local>
2019-04-10 21:43:09 -0300

    another approach

--- JPA-Employee-Basic/src/main/java/employee/presentation/EmployeeMB.java
+++ JPA-Employee-Basic/src/main/java/employee/presentation/EmployeeMB.java
@@ -6,8 +6,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -15,20 +16,18 @@ import employee.Employee;
 import employee.data.EmployeeDAO;
 
 @Named(value = "employeeManagedBean")
-//@ViewScoped
-@RequestScoped
+@ViewScoped
 public class EmployeeMB implements Serializable {
 	private static final long serialVersionUID = -1072401845049198412L;
 
 	@Inject EmployeeDAO dao;
 	
 	//Auxiliary fields for JSF
-	private List<Employee> employeeList = new ArrayList<>();
+	private List<Employee> employeeList;
 	private Employee employee = new Employee();
+	private transient DataModel<Employee> model = null;
 	
 	public List<Employee> getEmployeeList() {
-		System.out.println("listar usuariosa");
-		employeeList = dao.findEmployees();
 		return employeeList;
 	}
 	
@@ -55,11 +54,15 @@ public class EmployeeMB implements Serializable {
 		return "employeelist";
 	}
 	
-	public String removeEmployee() {
-		System.out.println("remover usuario");
-//		dao.removeEmployee(employee);
-//		employeeList = dao.findEmployees();
-		return "employeelist";
+	public DataModel<Employee> getModel() {
+		if (model == null) {
+			model = new ListDataModel<Employee>(employeeList);
+		}
+		
+		return model;
 	}
 	
+	public void delete() {
+		dao.removeEmployee(model.getRowData());
+	}
 }
